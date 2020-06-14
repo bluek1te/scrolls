82bbe44012695ce7eff9d1242c6a211fa8353df6
bluek1te <phillip.qtr@gmail.com>
2020-06-14 04:31:53 +0000

    Testing what tags look like + title bar

--- src/App.vue
+++ src/App.vue
@@ -1,99 +1,89 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated class="glossy">
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          @click="leftDrawerOpen = !leftDrawerOpen"
-          aria-label="Menu"
-          icon="menu"
-        />
+  <div class="q-pa-md">
+    <div class="q-pa-md">
+    <q-toolbar class="bg-blue text-white shadow-2 rounded-borders">
+      <q-btn flat label="bluek1te" />
+      <q-space />
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
+      <!--
+        notice shrink property since we are placing it
+        as child of QToolbar
+      -->
+      <q-tabs v-model="tab" shrink>
+        <q-tab name="tab1" label="Tab 1" />
+        <q-tab name="tab2" label="Tab 2" />
+        <q-tab name="tab3" label="Tab 3" />
+      </q-tabs>
+    </q-toolbar>
+  </div>
+    <div class="row">
+      <div class="col">
+        .col
+      </div>
+      <div class="col">
+        .col
+      </div>
+    </div>
+    <div class="row">
+      <div v-for="tag in tags" :key="tag.item">
+         <q-badge>
+           {{ tag.item }}
+         </q-badge>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        .col
+      </div>
+      <div class="col">
+        .col
+      </div>
+      <div class="col">
+        .col
+      </div>
+    </div>
 
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
-    </q-header>
-
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      content-class="bg-grey-2"
-    >
-      <q-list>
-        <q-item-label header>Essential Links</q-item-label>
-        <q-item clickable tag="a" target="_blank" href="https://quasar.dev">
-          <q-item-section avatar>
-            <q-icon name="school" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Docs</q-item-label>
-            <q-item-label caption>quasar.dev</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item clickable tag="a" target="_blank" href="https://github.com/quasarframework/">
-          <q-item-section avatar>
-            <q-icon name="code" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Github</q-item-label>
-            <q-item-label caption>github.com/quasarframework</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item clickable tag="a" target="_blank" href="https://chat.quasar.dev">
-          <q-item-section avatar>
-            <q-icon name="chat" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Discord Chat Channel</q-item-label>
-            <q-item-label caption>chat.quasar.dev</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item clickable tag="a" target="_blank" href="https://forum.quasar.dev">
-          <q-item-section avatar>
-            <q-icon name="forum" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Forum</q-item-label>
-            <q-item-label caption>forum.quasar.dev</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item clickable tag="a" target="_blank" href="https://twitter.com/quasarframework">
-          <q-item-section avatar>
-            <q-icon name="rss_feed" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Twitter</q-item-label>
-            <q-item-label caption>@quasarframework</q-item-label>
-          </q-item-section>
-        </q-item>
-      </q-list>
-    </q-drawer>
-
-    <q-page-container>
-      <HelloWorld />
-    </q-page-container>
-  </q-layout>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'LayoutDefault',
 
   components: {
-    HelloWorld
   },
 
   data () {
     return {
-      leftDrawerOpen: false
+      leftDrawerOpen: false,
+      tags: [
+         { item: 'linux', type: 'sys' },
+         { item: 'windows', type: 'sys' },
+         { item: 'wireshark', type: 'software' },
+         { item: 'eagle', type: 'software' },
+         { item: 'c++', type: 'language' },
+         { item: 'c', type: 'language' },
+         { item: 'bash', type: 'language' },
+         { item: 'c#', type: 'language' },
+         { item: 'javascript', type: 'language' },
+         { item: 'bamboo' , type: 'software' },
+         { item: 'python', type: 'language' },
+         { item: 'vue', type: 'framework' },
+         { item: 'flask', type: 'framework' },
+         { item: 'sqlite', type: 'db' },
+         { item: 'docker', type: 'sys' },
+         { item: 'yocto', type: 'framework' },
+         { item: 'postgresql', type: 'db' },
+         { item: 'photoshop', type: 'software' },
+         { item: 'git', type: 'git' },
+         { item: 'gdb', type: 'software' },
+         { item: 'valgrind', type: 'software' },
+         { item: 'bitbucket', type: 'software' },
+         { item: 'confluence', type: 'software' },
+         { item: 'github', type: 'software' },
+         { item: 'excel', type: 'software' },
+      ]
     }
   }
 }
