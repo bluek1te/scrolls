fd862c894d02425f67b24827a68db60482dec67a
bluek1te <phillip.qtr@gmail.com>
2020-06-21 05:08:35 +0000

    Changed font for title label

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './quasar'
+import '@/app.css'
 
 Vue.config.productionTip = false
 
--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,10 @@
-const GoogleFontsPlugin = require("google-fonts-webpack-plugin")
-
 module.exports = {
   pluginOptions: {
     quasar: {
       importStrategy: 'kebab',
       rtlSupport: false
     }
-  },
-  configureWebpack: {
-    plugins: [
-       new GoogleFontsPlugin({
-          fonts: [
-	    { family: "Lora" }
-          ]
-       })
-    ]
-  },
+  },    
   transpileDependencies: [
     'quasar'
   ]
