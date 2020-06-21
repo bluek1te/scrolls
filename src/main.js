ca98a14c886a1899c8f1db46410c8797cc7f8baa
bluek1te <phillip.qtr@gmail.com>
2020-06-21 07:03:50 +0000

    Adding routing

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ import App from './App.vue'
 import './quasar'
 import '@/app.css'
 
+import router from './router'
+
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
